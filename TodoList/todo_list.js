d01ff7aeb676e8f3fd95e77b529ff2a4b16649da
jose marte <tumusicalatina@gmail.com>
2019-03-30 11:57:57 -0400

    added a stack data structure in JS

--- TodoList/todo_list.js
+++ TodoList/todo_list.js
@@ -1,27 +1,12 @@
 var todolist = {
   todos: [],
-  displayTodos: function() {
-    if(this.todos.length === 0){
-        console.log("Your todo list is empty!")
-    }else{
-        console.log("My Todos: ");
-        for(var i = 0; i < this.todos.length; i++){
-           // console.log(this.todos[i].todoText);
 
-            if(this.todos[i].completed === true){
-              console.log("(X)",this.todos[i].todoText);
-            }else{
-              console.log("( )",this.todos[i].todoText);
-            }
-        }
-    }
-  },
   addTodo: function(todoText){
     this.todos.push({
       todoText: todoText,
       completed: false
     });
-    this.displayTodos();
+    
   },
   changeTodo: function(position, todoText){
     this.todos[position].todoText = todoText;
@@ -29,12 +14,12 @@ var todolist = {
   },
   deleteTodo: function(position){
     this.todos.splice(position,1);
-    this.displayTodos();
+    
   },
   toggleCompleted: function(position){
     var todo = this.todos[position];
     todo.completed = !todo.completed;
-    this.displayTodos();
+    
   },
   toggleAll: function(){
       var totalTodos = this.todos.length;
@@ -64,23 +49,22 @@ var todolist = {
 
           }//end for
       }
-      this.displayTodos();
+      
   }//end function
   
 };
 
 var handlers = {
-  displayTodos: function(){
-    todolist.displayTodos();
-  },
 
   toggleAll: function(){
     todolist.toggleAll();
+    view.displayTodos();
   },
   addTodo: function(){
     var addTodoTextInput = document.getElementById("addTodoTextInput");
     todolist.addTodo(addTodoTextInput.value);
     addTodoTextInput.value ="";
+    view.displayTodos();
   },
   changeTodo: function(){
     var changeTodoPositionInput = document.getElementById("changeTodoPositionInput");
@@ -90,12 +74,46 @@ var handlers = {
     //clear the inputs fields
     changeTodoPositionInput.value = "";
     changeTodoTextInput.value = "";
+    view.displayTodos();
     
   },
   deleteTodo: function(){
     var deleteTodoPositionInput = document.getElementById("deleteTodoPositionInput");
     todolist.deleteTodo(deleteTodoPositionInput.valueAsNumber);
     deleteTodoPositionInput.value = "";
-  }
+    view.displayTodos();
+  },
+  toggleCompleted: function(){
+  var toggleCompletedPositionInput = document.getElementById("toggleCompletedPositionInput");
+  todolist.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
 
+  toggleCompletedPositionInput.value = "";
+  view.displayTodos();
+
+}
+
+};
+
+var view = {
+displayTodos: function() {
+  var todosUl = document.querySelector("ul");
+  todosUl.innerHTML = "";
+  for(var i = 0; i < todolist.todos.length; i++){
+    var todoLi = document.createElement("li");
+    var todo = todolist.todos[i];
+    var todoTextWithCompletion ="";
+
+    if(todo.completed === true){
+      todoTextWithCompletion = "(X) " + todo.todoText;
+    }else{
+      todoTextWithCompletion = "( ) " + todo.todoText;
+
+    }//end if
+
+    todoLi.textContent = todoTextWithCompletion;
+
+    todosUl.appendChild(todoLi);
+
+  }//end for
+}
 };
