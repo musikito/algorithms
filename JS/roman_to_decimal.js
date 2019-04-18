97fb9c7f652d4db80fe6a5ee9707e87a7bc590bf
JoséMiguel Marte <tumusicalatina@gmail.com>
2019-04-18 16:09:32 -0400

    Create roman_to_decimal.js

---
+++ JS/roman_to_decimal.js
@@ -0,0 +1,16 @@
+function fromRoman(str) {  
+  var result = 0;
+  // the result is now a number, not a string
+  var decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var roman = ["M", "CM","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"];
+  for (var i = 0;i<=decimal.length;i++) {
+    while (str.indexOf(roman[i]) === 0){
+    //checking for the first characters in the string
+      result += decimal[i];
+      //adding the decimal value to our result counter
+      str = str.replace(roman[i],'');
+      //remove the matched Roman letter from the beginning
+    }
+  }
+  return result;
+}
